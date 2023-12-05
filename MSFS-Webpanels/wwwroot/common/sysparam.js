4f5111d306cce7aa5d01e08ebe5f58d8dae8e3be
K.K. Lai <lai.kaikwong@gmail.com>
2023-12-05 10:46:51 +0800

    fix url

--- MSFS-Webpanels/wwwroot/common/sysparam.js
+++ MSFS-Webpanels/wwwroot/common/sysparam.js
@@ -5,12 +5,11 @@ require.config({
 define(function() {           
     return {
         isOfflineTest : false,
-        //isOfflineTest : false,
         defaultCoolDown : 500, // ms
         refreshPeriod : 50, // ms;
         serverUpdateCooldown :20, // ms  
-        //simVarUrl: 'sample-data.json',
-        simVarUrl: 'http://192.168.2.195:8888/api/SimData',
+        simVarUrl: '/api/SimData',
+        mapDataUrl: '/api/MapData',
         tapEvent: ('ontouchstart' in document.documentElement) ? 'touchstart': 'mousedown',
         tapEndEvent: ('ontouchstart' in document.documentElement) ? 'touchend': 'mouseup mouseleave',
         dragEvent: ('ontouchstart' in document.documentElement) ? 'touchstart touchmove touchend': 'mousedown mousemove mouseup mouseleave'
