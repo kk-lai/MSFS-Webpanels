572e4638eeeefd6d74b3384e04d6ffc4c19435b8
Kevin King <lai.kaikwong@gmail.com>
2025-09-19 19:45:39 +0800

    Update version code

--- MSFS-Webpanels/wwwroot/c172/const.js
+++ MSFS-Webpanels/wwwroot/c172/const.js
@@ -5,6 +5,6 @@ define(function () {
         refreshPeriod : 50, // ms;
         serverUpdateCooldown :20, // ms  
         simVarUrl: '/api/SimData',
-        versionCode: '1.1.2'
+        versionCode: '1.1.3'
     }
 });
